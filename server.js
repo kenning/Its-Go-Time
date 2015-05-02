8d605c4e5c53f4b04dfe56af408d7e7f8126111b
kenning <hwerzog@gmail.com>
2015-05-02 13:39:05 -0700

    Forgot to change newserver.js to server.js

---
+++ server.js
@@ -0,0 +1,39 @@
+var restify = require('restify');
+var client = restify.createJsonClient({
+  url: 'https://hooks.slack.com/services/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',
+  version: '*'
+});
+
+// var monk = require('monk');
+// var mongodb = require('mongodb');
+// var connectionString = process.env.CUSTOMCONNSTR_MONGOLAB_URI;
+// var db = monk(connectionString);
+// var users = db.get('userlist');
+// urlList.findOne({Unused:true}, function(err, newImgListDoc){
+
+// https://github.com/kenning/hinternet/blob/master/routes/index.js
+
+function respond(req, res, next) {
+  postText('asdf');
+  res.send('hello ' + req.params.name);
+  next();
+}
+
+var postText = function(text) {
+    client.post('https://hooks.slack.com/services/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',
+      { 'text': 'azure really works' }, function(err, req, res, obj) {
+    if(err) console.log(err);
+    console.log('%d -> %j', res.statusCode, res.headers);
+    console.log('%j', obj);
+  });
+}
+
+
+var server = restify.createServer();
+server.get('/hello/:name', respond);
+server.head('/hello/:name', respond);
+
+server.listen(8080, function() {
+  console.log('%s listening at %s', server.name, server.url);
+});
+
