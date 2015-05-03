cca5e0205f16c2d2eb7ac931722f78e0edb74819
kenning <hwerzog@gmail.com>
2015-05-02 21:20:56 -0700

    halp

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ var games = {};
 //http://www.fileformat.info/info/unicode/char/25ef/index.htm
 
 server.post('/', function(req, res) {
-  var coolText = JSON.stringify(req.body);
+  var coolText = JSON.stringify(req.body['channel_name']);
   res.send(201, {'text': coolText});
 });
 server.listen(port, function() {
