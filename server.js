a6cfebb75578da79b62a8594c4d5525329b06dba
kenning <hwerzog@gmail.com>
2015-05-03 12:50:27 -0700

    finally
     Please enter the commit message for your changes. Lines starting

--- server.js
+++ server.js
@@ -61,7 +61,7 @@ server.post('/', function(req, res) {
   // request.on('end', function () {
     var coolText = JSON.stringify(Object.keys(req.params));
     // var bestText = req.body.user_name + req.body.text;
-    res.send(201, {'text': coolText});
+    res.send(201, {'text': req.params.text});
   // }
   // var keys = Object.keys(req);
 
